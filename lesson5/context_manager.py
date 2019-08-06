3be853aab87c20b4ebe801466d9ca0e8d59282a4
dkarakuts <denys.karakuts@amadeus.com>
2019-08-06 20:47:46 +0300

    4th lesson task

---
+++ lesson5/context_manager.py
@@ -0,0 +1,38 @@
+# class File:
+#     def __init__(self, a, b):
+#         self._a = a
+#         self._b = b
+#
+#     def __enter__(self):
+#         print(f"Entered args are {self._a, self._b}")
+#         return "Hello"
+#
+#     def __exit__(self, *args):
+#         print("The end of context manager")
+#         print(args)
+#
+#
+# with File(1, 2) as f:
+#     print(f)
+
+
+class File:
+    def __init__(self, filename, regime):
+        self.filename = filename
+        self.regime = regime
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, self.regime)
+        return self.file
+
+    def __exit__(self, *args):
+        try:
+            self.file.close()
+        except Exception:
+            print(args[0])
+
+
+with File("Aaaa.txt", "w") as f:
+    print(1/0)
+    f.write('Hello Earth!')
