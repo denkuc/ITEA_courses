6a1004534853125bc603fe3f8075dc5095ef5f0b
dkarakuts <denys.karakuts@amadeus.com>
2019-08-20 13:00:59 +0300

    7th lesson after review

--- lesson7/context_manager_mysql.py
+++ lesson7/context_manager_mysql.py
@@ -1,39 +1,28 @@
 from sqlalchemy.dialects import mysql
 
 
-class MysqlContextManager:
+class CursorMysqlContextManager:
     def __init__(self, config):
         self.__config = config
 
     def __enter__(self):
         self.__connection = mysql.connector.connect(self.__config)
-        return self.__connection
-
-    def __exit__(self, *args):
-        self.__connection.close()
-
-
-class CursorContextManager:
-    def __init__(self, con):
-        self.__connection = con
-
-    def __enter__(self):
         self.__cursor = self.__connection.cursor()
-        return self.__cursor
+        return self.__connection, self.__cursor
 
     def __exit__(self, *args):
         self.__cursor.close()
+        self.__connection.close()
 
 
-config = {'user': 'root',
-          'password': 'denkuc',
-          'host': '127.0.0.1',
-          'database': 'employees'}
+config_dict = {'user': 'root',
+               'password': 'denkuc',
+               'host': '127.0.0.1',
+               'database': 'students'}
 
-query = """SELECT * FROM employees"""
+query = """SELECT * FROM student"""
 
-with MysqlContextManager(config) as con:
-    with CursorContextManager(con) as cursor:
-        cursor.execute(query)
-        query_results = cursor.fetchall()
-        print(query_results)
+with CursorMysqlContextManager(config_dict) as (cursor, connection):
+    cursor.execute(query)
+    query_results = cursor.fetchall()
+    print(query_results)
